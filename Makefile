cd01bed47fd70f318d0cbe710e62e9121c4f07a1
Xulang Wan <wan.xulang@grabtaxi.com>
2022-01-24 15:58:37 +0800

    update

---
+++ Makefile
@@ -0,0 +1,5 @@
+create-env:
+	virtualenv -p `which python3.7` env
+
+install-dependencies:
+	pip install -r requirements.txt
---
+++ src/graph/dsu.py
@@ -0,0 +1,31 @@
+class DSU:
+    def __init__(self, N: int):
+        # every node's init parent is itself
+        self.parent = list(range(N))
+
+        # size of each component
+        self.rank = [0] * N
+
+    def find(self, x: int) -> int:
+        if self.parent[x] != x:
+            parent = self.find(self.parent[x])
+            # compress the path
+            self.parent[x] = parent
+            return self.parent[x]
+        else:
+            return self.parent[x]
+
+    def union(self, x: int, y: int) -> bool:
+        xr, yr = self.find(x), self.find(y)
+        if xr == yr:
+            return False
+        else:
+            # merge to the parent with larger rank
+            if self.rank[xr] >= self.rank[yr]:
+                self.parent[yr] = xr
+                self.rank[xr] += self.rank[yr]
+                self.rank[yr] = 0
+            else:
+                self.parent[xr] = yr
+                self.rank[yr] += self.rank[xr]
+                self.rank[xr] = 0
