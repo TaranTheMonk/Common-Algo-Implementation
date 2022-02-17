816ca1c8dcc72dec2701326720bae816a0e9e709
Xulang Wan <wan.xulang@grabtaxi.com>
2022-02-17 17:32:02 +0800

    add documentation

--- src/graph/dsu.py
+++ src/graph/dsu.py
@@ -1,10 +1,22 @@
 class DSU:
-    def __init__(self, N: int):
+    def __init__(self, n: int):
+        """
+        1. only when dsu.parent[n] == n, n is the root!
+        2. set(dsu.parent) doesn't represent to all unique components:
+            dsu.parent -> [0, 0, 1]
+            dsu.parent[2] -> 1
+            dsu.parent[1] -> 0
+            dsu.parent[0] -> 0
+
+            set(dsu.parent) has 2 elements, but there's only one component!
+        :param n:
+        """
+
         # every node's init parent is itself
-        self.parent = list(range(N))
+        self.parent = list(range(n))
 
         # size of each component
-        self.rank = [1] * N
+        self.rank = [1] * n
 
     def find(self, x: int) -> int:
         # if x is not the root
