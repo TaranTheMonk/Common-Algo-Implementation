abacbbc8ab8d6875a6ec42781d0fc472c5a59598
Xulang Wan <taran.xulang.wan@gmail.com>
2022-04-03 12:16:57 +0800

    update type annotations

--- src/tree/trie.py
+++ src/tree/trie.py
@@ -1,15 +1,19 @@
+from typing import List, Optional
+
+
 class TrieNode:
     def __init__(self):
-        self.children = [None] * 26
+        self.children: List[Optional["TrieNode"]] = [None] * 26
         self.is_end = False
 
 
 class Trie:
     def __init__(self):
         self.root = TrieNode()
+        self._ch_start_idx = ord("a")
 
     def _char_to_index(self, c: str):
-        return ord(c) - ord('a')
+        return ord(c) - self._ch_start_idx
 
     def insert(self, key: str):
         node = self.root
@@ -24,21 +28,7 @@ class Trie:
         # mark last node as leaf
         node.is_end = True
 
-    def search_prefix(self, key):
-        """
-        search if key is defined as a prefix
-        """
-        node = self.root
-        for level in range(len(key)):
-            index = self._char_to_index(key[level])
-
-            if not node.children[index]:
-                return False
-            node = node.children[index]
-
-        return True
-
-    def search(self, key):
+    def search(self, key: str) -> bool:
         node = self.root
         for level in range(len(key)):
             index = self._char_to_index(key[level])
